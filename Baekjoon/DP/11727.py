067f55d847895ec2b3ada49869b723e31a911179
kmseunh <tmdgus8779@gmail.com>
2023-08-22 14:53:36 +0900

    백준: 2×n 타일링 2

---
+++ Baekjoon/DP/11727.py
@@ -0,0 +1,16 @@
+x = int(input())
+d = [0] * 1001
+
+
+def dp(x):
+    if x == 1:
+        return 1
+    if x == 2:
+        return 3
+    if d[x] != 0:
+        return d[x]
+    d[x] = (dp(x - 1) + 2 * dp(x - 2)) % 10007
+    return d[x]
+
+
+print(dp(x))
